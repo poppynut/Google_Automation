01876cac3b0c1da5f5e0ccb4d8567ebcddc21ad6
rajprabhuin@gmail.com <Aaditya8#>
2022-02-07 12:26:46 +0530

    Updated all the codes on 7thfeb

---
+++ Repeat_Crash_Course_Python_7Feb/week1.py
@@ -0,0 +1,36 @@
+#Functions ( )
+#Keywords ( Core Part of language and can be used only for syntax )
+# In programming any text without quotation marks is considered part of the code.
+
+print("Hello, World!")
+print("I'm programming in Python!")
+######################################################
+nameof=input("Please Enter the name of the User: ")
+print("Hello, " + nameof)
+
+
+for name in ["Joe","Stella","Kumar"]:
+    print("Hi "+ name)
+
+color = "White"
+thing = "America"
+print(color + " is the color of " + thing)
+
+print(4+5)
+print(7*9)
+print(1/3)
+print(((2050/5)-32))
+
+print((((1+2)*3)/4)**5)
+
+number=1000
+print((number/5)+30-45)
+
+ratio = ((1 + 5**(1/2))/2)
+print(ratio)
+
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size/sector_size
+
+print(sector_amount)
---
+++ Repeat_Crash_Course_Python_7Feb/week2.py
@@ -0,0 +1,119 @@
+######################
+#Variables, Expressions, Conditional Blocks####
+
+
+##DataTypes##
+#Text between Quotes is a String. String is a Datatype##
+#Integer is a Datatype## String, Integer, Float####
+print(7+7)
+print("7"+"8")
+print(7+8)
+
+print(7+8)
+######################
+a=7+7
+
+print(type('b'))
+print(type(a))
+print(type(2.5))
+
+#print("1234"+5678)
+#######################
+#Variables in Python##
+#Containers for Data.#
+#Expression is a combination of num, symbols and other variables which on evaluation produces a result##
+#Can not have a variable with the name print#
+#Variable names can not start. Variables are case sensitive##
+# 
+length=10
+width=2
+area=length*width
+print(area)
+print(7+8.5)
+#Implicit Cnversion of DataTypes. Intepreter automatically converts one datatype into another.####
+print("a"+"b"+"c")
+#######################
+print("This "+ "is "+"pretty " + "neat!")
+#It is possible to combine string and a number##
+#Only with explicit conversion##
+
+base=6
+height=3
+area=(base*height)/2
+
+print("The area of the Triangle is: " + str(area))
+
+#print("1234"+5678)
+#######################
+
+total = 2048 + 4537 + 97658 + 125 + 8
+files = 5
+average = total / files
+print("The average size is: " + str(average))
+#######################
+
+
+#Implicit Conversion == Automatically converts one DataType to another#
+#Explicit Conversion == Manually convert one Datatype to another##
+
+
+
+#######################
+bill = 47.28
+tip = bill * (15/100)
+total = bill + tip
+share = total/2 
+print("Each person needs to pay: " + str(share))
+#######################
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(int(result))
+#######################
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+
+print(word1  + ' ' + word2 + ' ' + word3 + ' ' + word4 + ' ' + word5 + ' ' + word6 + ' ' + word7)
+#######################
+
+
+
+
+#######################
+
+
+
+
+#######################
+
+
+
+
+
+#######################
+
+
+
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+
+
