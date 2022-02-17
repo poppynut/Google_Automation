d82642ca3bd150bf00558164d279d809d5b3e2e1
Santharam, Rajkumar <Rajkumar.Santharam@scientificgames.com>
2022-02-17 21:28:22 +0530

    Comminiting Scripts after Modifying

---
+++ Repeat_Crash_Course_Python_7Feb/List_DataType/dtlist6.py
@@ -0,0 +1,14 @@
+fruits=["Pineapple", "Banana", "Apple", "Melon"]
+fruits.append("Mango")
+print(fruits)
+fruits.append("Kiwi")
+print(fruits)
+fruits.insert(0, "orange")
+print(fruits)
+fruits.remove("Melon")
+print(fruits)
+fruits.pop(3)
+print(fruits)
+
+fruits[2]="StrawBerry"
+print(fruits)
---
+++ Repeat_Crash_Course_Python_7Feb/List_DataType/dtlist8.py
@@ -0,0 +1,12 @@
+def convert_seconds(seconds):
+    hours=seconds//3600
+    minutes=(seconds - hours * 3600) // 60
+    remaining_seconds=seconds-hours*3600-minutes*60
+    return hours, minutes, remaining_seconds
+
+result=convert_seconds(5000)
+type(result)
+print(result)
+
+hours, minutes, seconds = result
+print(hours, minutes, seconds)
