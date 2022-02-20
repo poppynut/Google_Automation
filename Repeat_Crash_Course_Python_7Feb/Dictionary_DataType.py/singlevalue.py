aadb69bcbbb5b173d5997b775ba99cc9c9ea21e5
rajprabhuin@gmail.com <Aaditya8#>
2022-02-20 11:02:37 +0530

    Updated

--- Repeat_Crash_Course_Python_7Feb/Dictionary_DataType.py/singlevalue.py
+++ Repeat_Crash_Course_Python_7Feb/Dictionary_DataType.py/singlevalue.py
@@ -1,4 +1,4 @@
 wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
-for cloths in wardrobe.keys():
+for clothes in wardrobe.keys():
 	for colors in wardrobe.values():
 		print("{} {}".format(colors, clothes))
