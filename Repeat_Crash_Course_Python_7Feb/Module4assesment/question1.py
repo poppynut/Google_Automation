fe13c6366fb97d60016e3f981656e3cbbd1f11b7
Santharam, Rajkumar <Rajkumar.Santharam@scientificgames.com>
2022-03-03 06:42:02 +0530

    Commiting Code

---
+++ Repeat_Crash_Course_Python_7Feb/Module4assesment/question1.py
@@ -0,0 +1,6 @@
+host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+print(host_addresses.keys())
+
+colors = ["red", "white", "blue"]
+colors.insert(2, "yellow")
+print(colors)
